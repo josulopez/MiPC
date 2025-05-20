4c41dd64f18090c4af2e42c2a6fa7d3912290db9
Josué López <josue.lopez.21005@gmail.com>
2025-05-20 15:33:31 -0600

    views funcionales

--- Tablas/views.py
+++ Tablas/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Usuario
+from .models import Producto
 # def nombre de la pagina
-def clienteslista(request):
+  
+def clientes(request):
   misclientes = Usuario.objects.all().values()
   template = loader.get_template('clientes.html')
   context = {
@@ -18,13 +20,41 @@ def details(request, id):
   }
   return HttpResponse(template.render(context, request))
 
-def Clientes(request):
-  template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+def juegos(request):
+  juegos = Producto.objects.filter(categoria__nombre__icontains='juegos')
+  context = {
+    "juegos" : juegos
+  }
+  template = loader.get_template('juegos.html')
+  return HttpResponse(template.render(context,request))
+
+def componentes(request):
+  componentes = Producto.objects.filter(categoria__nombre__icontains='componentes')
+  context = {
+    "componentes" : componentes
+  }
+  template = loader.get_template('componentes.html')
+  return HttpResponse(template.render(context,request))
+
+def licencias(request):
+  licencias = Producto.objects.filter(categoria__nombre__icontains='licencias')
+  context = {
+    "licencias" : licencias
+  }
+  template = loader.get_template('licencias.html')
+  return HttpResponse(template.render(context,request))
 
 def Main(request):
+  videojuegos = Producto.objects.filter(categoria__nombre__icontains='juegos')
+  componentes = Producto.objects.filter(categoria__nombre__icontains='componentes')
+  licencias = Producto.objects.filter(categoria__nombre__icontains='licencias')
+  context = {
+      "videojuegos": videojuegos,
+      "componentes": componentes,
+      "licencias": licencias,
+  }
   template = loader.get_template('main.html')
-  return HttpResponse(template.render())
+  return HttpResponse(template.render(context, request))
 
 def testing(request):
   clientes = Usuario.objects.all().values()
